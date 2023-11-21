564b5021ff66dde32e00f9e0f3b7814c1f728190
KrzysztofHerman <herman@ihp-microelectronics.com>
2023-11-21 08:25:54 +0100

    spi design altered
    Signed-off-by: KrzysztofHerman <herman@ihp-microelectronics.com>

--- flow/designs/ihp-sg13g2/spi/config.mk
+++ flow/designs/ihp-sg13g2/spi/config.mk
@@ -5,7 +5,12 @@ export VERILOG_FILES = ./designs/src/$(DESIGN_NICKNAME)/spi.v
 export SDC_FILE      = ./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/constraint.sdc
 
 export USE_FILL = 1
+export FLOORPLAN_DEF = ./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/ihp130-spi.def
+export ADDITIONAL_LEFS = ./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/IOLib.lef
+
+export DIE_AREA = 0.0 0.0 1000.0 1000.0
+export CORE_AREA = 250 250 750 750
 
 export PLACE_DENSITY ?= 0.88
-export CORE_UTILIZATION = 20
+#export CORE_UTILIZATION = 20
 export TNS_END_PERCENT = 100
--- flow/designs/src/spi/spi.v
+++ flow/designs/src/spi/spi.v
@@ -22,6 +22,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 module spi(
+    inout Vss,
+    inout Vdd,
     input clk,
     input rst,
     input ss,
